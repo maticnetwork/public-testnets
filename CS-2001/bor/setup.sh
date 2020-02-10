0bf732471ae1a158afe1449894364bd4b38b7d79
delroy.bosco@gmail.com <delroy.bosco@gmail.com>
2020-02-11 01:11:29 +0530

    setup script

---
+++ CS-2001/bor/setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+DATA_DIR=$PWD/dataDir
+BUILD_DIR=$GOPATH/src/github.com/maticnetwork/bor/build/bin
+
+# set -x #echo on
+
+$BUILD_DIR/bor --datadir $DATA_DIR init ./genesis.json
+
+cp ./static-nodes.json $DATA_DIR/bor/static-nodes.json
+
+mkdir -p logs
+
+echo "Setup done!"
--- CS-2001/bor/start.sh
+++ CS-2001/bor/start.sh
@@ -10,11 +10,7 @@ ADDRESS=$1
 DATA_DIR=$PWD/dataDir
 BUILD_DIR=$GOPATH/src/github.com/maticnetwork/bor/build/bin
 
-set -x #echo on
-
-$BUILD_DIR/bor --datadir $DATA_DIR init ./genesis.json
-
-cp ./static-nodes.json $DATA_DIR/bor/static-nodes.json
+# set -x #echo on
 
 mkdir -p logs
 
